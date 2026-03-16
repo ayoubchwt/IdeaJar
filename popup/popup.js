77216ce30667eb2c3db9ba68e9a24b7b6df70b5b
eclipse <ayoub.chouat@esprit.tn>
2026-03-16 22:57:11 +0100

    added the forms and the overall navigation while following the SPA concept

--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,16 @@
 const ToggleStatsBtn = document.getElementById("toggle-stats");
 const Stats = document.getElementById("content-stats");
-const main = document.querySelector("main");
+const homePage = document.getElementById("home-page");
 const prompts = document.getElementById("prompts");
+const addButton = document.getElementById("add-button");
+const addForm = document.getElementById("add-form");
+const editFrom = document.getElementById("edit-form");
 ToggleStatsBtn.addEventListener("click", () => {
   Stats.classList.toggle("hidden");
-  main.classList.toggle("main-expanded");
+  homePage.classList.toggle("home-page-expanded");
   prompts.classList.toggle("prompts-expanded");
+});
+addButton.addEventListener("click", () => {
+  homePage.style.display = "none";
+  addForm.style.display = "flex";
 });
