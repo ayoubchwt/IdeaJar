85af2a62220f810a4ac0e757224bd3ad8b765542
eclipse <ayoub.chouat@esprit.tn>
2026-03-17 07:49:48 +0100

    finished the add form for the prompts

--- popup/popup.js
+++ popup/popup.js
@@ -1,16 +1,19 @@
-const ToggleStatsBtn = document.getElementById("toggle-stats");
-const Stats = document.getElementById("content-stats");
-const homePage = document.getElementById("home-page");
-const prompts = document.getElementById("prompts");
+// buttons
+const ToggleStatsButton = document.getElementById("toggle-stats");
 const addButton = document.getElementById("add-button");
-const addForm = document.getElementById("add-form");
-const editFrom = document.getElementById("edit-form");
-ToggleStatsBtn.addEventListener("click", () => {
+// components
+const Stats = document.getElementById("content-stats");
+const prompts = document.getElementById("prompts");
+// pages
+const homePage = document.getElementById("home-page");
+const addPage = document.getElementById("add-page");
+const editPage = document.getElementById("edit-page");
+ToggleStatsButton.addEventListener("click", () => {
   Stats.classList.toggle("hidden");
   homePage.classList.toggle("home-page-expanded");
   prompts.classList.toggle("prompts-expanded");
 });
 addButton.addEventListener("click", () => {
   homePage.style.display = "none";
-  addForm.style.display = "flex";
+  addPage.style.display = "flex";
 });
