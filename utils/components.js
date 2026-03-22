ef40039ea6cd8b27e2b2e753be0d6ae826f3beb3
eclipse <ayoub.chouat@esprit.tn>
2026-03-22 15:09:09 +0100

    added and polished the export / import drop down menu

--- utils/components.js
+++ utils/components.js
@@ -1,4 +1,6 @@
 const favoriteFilter = document.getElementById("favorite-filter");
+const backupContainer = document.getElementById("backup-container");
+const backupTrigger = document.getElementById("backup-trigger");
 const filterTrigger = document.getElementById("filter-trigger");
 const allOption = document.getElementById("all-option");
 const favoriteOption = document.getElementById("favorite-option");
@@ -6,9 +8,14 @@ const filterTriggerLabel = document.getElementById("filter-trigger-label");
 filterTrigger.addEventListener("click", () => {
     favoriteFilter.classList.toggle("open");
 });
+backupTrigger.addEventListener("click", () => {
+    backupContainer.classList.toggle("open");
+});
 document.addEventListener("click", (e) => {
     if (!favoriteFilter.contains(e.target))
         favoriteFilter.classList.remove("open");
+    if (!backupContainer.contains(e.target))
+        backupContainer.classList.remove("open");
 });
 allOption.addEventListener("click", () => {
     filterTriggerLabel.textContent = "Show All";
