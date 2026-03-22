977f4e625069cbb0f7b4e88a1c62f5207273380d
eclipse <ayoub.chouat@esprit.tn>
2026-03-22 10:21:37 +0100

    made the tigger label follow the drop down action and polished the span so it wont be squeezed

--- utils/components.js
+++ utils/components.js
@@ -1,6 +1,21 @@
 console.log("am inside components");
 const favoriteFilter = document.getElementById("favorite-filter");
 const filterTrigger = document.getElementById("filter-trigger");
+const allOption = document.getElementById("all-option");
+const favoriteOption = document.getElementById("favorite-option");
+const filterTriggerLabel = document.getElementById("filter-trigger-label");
 filterTrigger.addEventListener("click", () => {
     favoriteFilter.classList.toggle("open");
+});
+document.addEventListener("click", (e) => {
+    if (!favoriteFilter.contains(e.target))
+        favoriteFilter.classList.remove("open");
+});
+allOption.addEventListener("click", () => {
+    filterTriggerLabel.textContent = "Show All";
+    favoriteFilter.classList.remove("open");
+});
+favoriteOption.addEventListener("click", () => {
+    filterTriggerLabel.textContent = "Favorite";
+    favoriteFilter.classList.remove("open");
 });
